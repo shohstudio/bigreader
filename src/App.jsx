95b93817d6e1361e995783e2ac436de7e628362b
Shoyatbek <shakhinmukhammed@gmail.com>
2025-12-25 00:37:18 +0500

    Fix: Switch to HashRouter for GitHub Pages support
    - Replaced BrowserRouter with HashRouter in App.jsx.
    - This resolves 404 errors on page refresh by leveraging hash-based navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
@@ -8,7 +8,7 @@ import BookDetails from './pages/BookDetails';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
